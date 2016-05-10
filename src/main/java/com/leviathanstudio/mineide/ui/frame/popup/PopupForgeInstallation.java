0cfd7c7f1ebb74d87c361e00c0d5c2ef0b8da42c
Ourten <ourtencoop@gmail.com>
2016-05-11 01:29:12 +0200

    Migrate all forge structure to singletons

--- src/main/java/com/leviathanstudio/mineide/ui/frame/popup/PopupForgeInstallation.java
+++ src/main/java/com/leviathanstudio/mineide/ui/frame/popup/PopupForgeInstallation.java
@@ -13,23 +13,27 @@ import javafx.scene.control.ButtonType;
 
 public class PopupForgeInstallation
 {
-    
+
     public static void showPopup()
     {
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle("Information");
-        alert.setHeaderText(Translation.LANG.getTranslation("alert.forge.reinstall.header") + Gui.mineIdeInfo.getForgeVersion());
-        alert.setContentText(Translation.LANG.getTranslation("alert.forge.reinstall.content.line1") + "\n" + Translation.LANG.getTranslation("alert.forge.reinstall.content.line2"));
-        
+        alert.setHeaderText(
+                Translation.LANG.getTranslation("alert.forge.reinstall.header") + Gui.mineIdeInfo.getForgeVersion());
+        alert.setContentText(Translation.LANG.getTranslation("alert.forge.reinstall.content.line1") + "\n"
+                + Translation.LANG.getTranslation("alert.forge.reinstall.content.line2"));
+
         ButtonType buttonForceUpdate = new ButtonType(Translation.LANG.getTranslation("button.forceUpdate"));
-        ButtonType buttonCancel = new ButtonType(Translation.LANG.getTranslation("button.cancel"), ButtonData.CANCEL_CLOSE);
-        
+        ButtonType buttonCancel = new ButtonType(Translation.LANG.getTranslation("button.cancel"),
+                ButtonData.CANCEL_CLOSE);
+
         alert.getButtonTypes().setAll(buttonForceUpdate, buttonCancel);
-        
+
         Optional<ButtonType> result = alert.showAndWait();
-        if(result.get() == buttonForceUpdate)
-            ForgeWorkspace.forceUpdate();
+        if (result.get() == buttonForceUpdate)
+            ForgeWorkspace.getInstance().forceUpdate();
         else
-        {}
+        {
+        }
     }
 }
