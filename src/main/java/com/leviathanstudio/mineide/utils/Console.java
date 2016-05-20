35f5e4cca4d9136abce7119fb187d24a38d94036
Superloup10 <Superloup10@gmail.com>
2016-05-20 08:48:38 +0200

    Add write log

--- src/main/java/com/leviathanstudio/mineide/utils/Console.java
+++ src/main/java/com/leviathanstudio/mineide/utils/Console.java
@@ -11,45 +11,51 @@ import javafx.scene.control.TextArea;
 public class Console extends PrintStream
 {
     private TextArea output;
-
+    
     public Console(TextArea out) throws FileNotFoundException
     {
-        super(new File(Util.LOG_DIR, getLogFileName()));
+        super(getLogName());
         this.output = out;
     }
-
+    
     @Override
     public void write(int b)
     {
-        addToConsole(new String(new char[] { (char) b }));
+        addToConsole(new String(new char[] {(char)b}));
     }
-
+    
     @Override
     public void write(byte[] buf, int off, int len)
     {
         addToConsole(new String(buf, off, len));
     }
-
+    
     @Override
     public void println(String x)
     {
         addToConsole(x);
     }
-
+    
     private void addToConsole(String text)
     {
-        this.output.setText(this.output.getText() + text + "\n");
+        final String logContent = this.output.getText() + text + "\n";
+        this.output.setText(logContent);
+        writeLog(logContent);
     }
-
-    public static String getLogFileName()
+    
+    public static File getLogName()
     {
         String res = "";
         LocalDate date = LocalDate.now();
         LocalTime time = LocalTime.now();
-
-        res += String.format("%1$04d%2$02d%3$02d%4$02d%5$02d%6$02d", date.getYear(), date.getMonth().getValue(),
-                date.getDayOfMonth(), time.getHour(), time.getMinute(), time.getSecond());
-
-        return res + ".log";
+        
+        res += String.format("%1$04d%2$02d%3$02d%4$02d%5$02d%6$02d", date.getYear(), date.getMonth().getValue(), date.getDayOfMonth(), time.getHour(), time.getMinute(), time.getSecond());
+        
+        return new File(Util.LOG_DIR, res + ".log");
+    }
+    
+    private void writeLog(String logContent)
+    {
+        Util.writeFile(getLogName(), logContent);
     }
 }
--- src/main/java/com/leviathanstudio/mineide/utils/Util.java
+++ src/main/java/com/leviathanstudio/mineide/utils/Util.java
@@ -1,10 +1,6 @@
 package com.leviathanstudio.mineide.utils;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,40 +8,44 @@ import com.leviathanstudio.mineide.json.MineIDEConfig;
 
 public class Util
 {
-    public static final String IMG_DIR                = "/mineIDE/img/";
-    public static final String CSS_DIR                = "/mineIDE/css/";
-    public static final String JSON_DIR               = "/mineIDE/json/";
-    public static final String HTML_DIR               = "/mineIDE/html/";
-    public static final String LANG_DIR               = "/mineIDE/lang/";
-    public static final String TEMPLATE_DIR           = "/minecraft/template/";
-
-    public static final File   ROOT                   = OSHelper.getWorkingDirectory();
-    public static final File   CONFIG                 = new File(Util.ROOT, "config");
-    public static final File   PROJECT                = new File(Util.ROOT, "project");
-    public static final File   LOG_DIR                = new File(Util.ROOT, "log/");
-    public static final File   FORGE_DIR              = new File(Util.ROOT + "/workspace/forge");
-    public static final File   FORGE_SRC_JAVA_DIR     = new File(Util.ROOT + "/workspace/forge/src/main/java");
-    public static final File   FORGE_SRC_RESOURCE_DIR = new File(Util.ROOT + "/workspace/forge/src/main/resources");
-
-    public static void writeFile(File filePath, String fileName, String fileContent, String fileExtension)
+    public static final String IMG_DIR = "/mineIDE/img/";
+    public static final String CSS_DIR = "/mineIDE/css/";
+    public static final String JSON_DIR = "/mineIDE/json/";
+    public static final String HTML_DIR = "/mineIDE/html/";
+    public static final String LANG_DIR = "/mineIDE/lang/";
+    public static final String TEMPLATE_DIR = "/minecraft/template/";
+    
+    public static final File ROOT = OSHelper.getWorkingDirectory();
+    public static final File CONFIG = new File(Util.ROOT, "config");
+    public static final File PROJECT = new File(Util.ROOT, "project");
+    public static final File LOG_DIR = new File(Util.ROOT, "log/");
+    public static final File FORGE_DIR = new File(Util.ROOT + "/workspace/forge");
+    public static final File FORGE_SRC_JAVA_DIR = new File(Util.ROOT + "/workspace/forge/src/main/java");
+    public static final File FORGE_SRC_RESOURCE_DIR = new File(Util.ROOT + "/workspace/forge/src/main/resources");
+    
+    public static void writeFile(File file, String fileContent)
     {
         try
         {
-            File file = new File(filePath + "/" + fileName + "." + fileExtension);
-
-            if (!file.exists())
+            if(!file.exists())
                 file.createNewFile();
-
+            
             FileWriter fw = new FileWriter(file.getAbsoluteFile());
             BufferedWriter bw = new BufferedWriter(fw);
             bw.write(fileContent);
             bw.close();
-        } catch (Exception e)
+        }
+        catch(IOException e)
         {
             e.printStackTrace();
         }
     }
-
+    
+    public static void writeFile(String filePath, String fileName, String fileContent, String fileExtension)
+    {
+        writeFile(new File(filePath, fileName + "." + fileExtension), fileContent);
+    }
+    
     public static void initDirectory()
     {
         checkDir(ROOT);
@@ -56,13 +56,13 @@ public class Util
         checkDir(FORGE_SRC_JAVA_DIR);
         checkDir(FORGE_SRC_RESOURCE_DIR);
     }
-
+    
     public static void checkDir(File file)
     {
-        if (!file.exists())
+        if(!file.exists())
             file.mkdirs();
     }
-
+    
     public static void replaceMappingSelected()
     {
         try
@@ -71,23 +71,23 @@ public class Util
             BufferedReader file = new BufferedReader(new FileReader(inputFile));
             String line;
             String input = "";
-
-            while ((line = file.readLine()) != null)
+            
+            while((line = file.readLine()) != null)
                 input += line + '\n';
-
+            
             file.close();
-
+            
             Pattern p = Pattern.compile("\\_(.*?)\\\"");
             Matcher m = p.matcher(input);
-            while (m.find())
-                input = input.replace("mappings = \"snapshot_" + m.group(1) + "\"",
-                        "mappings = " + "\"" + MineIDEConfig.getMappingVersion() + "\"");
-
+            while(m.find())
+                input = input.replace("mappings = \"snapshot_" + m.group(1) + "\"", "mappings = " + "\"" + MineIDEConfig.getMappingVersion() + "\"");
+            
             FileWriter fileOut = new FileWriter(inputFile);
             fileOut.write(input);
             fileOut.close();
-
-        } catch (Exception e)
+            
+        }
+        catch(Exception e)
         {
             System.out.println("Problem reading file.");
             e.printStackTrace();
